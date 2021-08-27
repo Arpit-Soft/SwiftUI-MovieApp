55aff227679f355fbeaaae68eb078a94c82833ff
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-28 00:54:13 +0530

    Initial Commit

---
+++ SwiftUI-MovieApp/SwiftUI_MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MovieAppApp.swift
+//  SwiftUI-MovieApp
+//
+//  Created by Arpit Dixit on 28/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
